84c32bf3b9dfda3114e940e93fc635d0d1ee7a98
janggalman <janggalman@gmail.com>
2020-06-21 13:46:23 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjoeun/numberbaseballgame/BaseActivity.kt
@@ -0,0 +1,11 @@
+package kr.tjoeun.numberbaseballgame
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
